0b6e8ef66048ced9331c93dabdeca2f56eb573b6
jyao <jing.yao@blackboard.com>
2019-06-17 12:56:40 +0800

    added new start java

---
+++ interview/src/main/java/vmware/interview/Quiz/VmwareQuizSolutionApplication.java
@@ -0,0 +1,13 @@
+package vmware.interview.Quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmwareQuizSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmwareQuizSolutionApplication.class, args);
+	}
+
+}
